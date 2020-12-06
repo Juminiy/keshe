3be55c84c1d35c63d95d5c07682dcb4a446f9e7f
Juminiy <2285151098@qq.com>
2020-12-06 21:30:55 +0800

    add http request

--- personInformation/httpRequestReceive.js
+++ personInformation/httpRequestReceive.js
@@ -2,13 +2,14 @@ function getPropertyByName(yearMonth){
     $.ajax({
         type: "post",
         url: "https://authorize.hulingnan.site:8124/keshe/getRecord",
-        data: { yearMonth:yearMonth},
+        data: { yearMonth:yearMonth.value},
         success : function(result){
             if ( result == "500" ){
                 console.log("server error")
             } else {
                 return result ;
             }
+            alert(yearMonth.value) ;
         }
     });
 }
@@ -18,8 +19,8 @@ function addRecord(yearMonth, income, foodConsumption,
     $.ajax({
         type: "post",
         url: "https://authorize.hulingnan.site:8124/keshe/addRecord",
-        data: { yearMonth:yearMonth,income:income,foodConsumption:foodConsumption, rent:rent,
-                childrenEducationCost:childrenEducationCost,WaterPowerCost:WaterPowerCost,MedicalCost:MedicalCost},
+        data: { yearMonth:yearMonth.value,income:income.value,foodConsumption:foodConsumption.value, rent:rent.value,
+                childrenEducationCost:childrenEducationCost.value,WaterPowerCost:WaterPowerCost.value,MedicalCost:MedicalCost.value},
         success : function(result){
             if ( result == "200" ){
                 alert("添加成功!") ;
