a962fd646553116d6bd93b5c6c420a31936c7f1c
Juminiy <2285151098@qq.com>
2020-12-07 22:29:35 +0800

    add http request

--- personInformation/httpRequestReceive.js
+++ personInformation/httpRequestReceive.js
@@ -4,10 +4,10 @@ function getPropertyByName(yearMonth){
         url: "https://authorize.hulingnan.site:8124/keshe/getRecord",
         data: { yearMonth:yearMonth.toString()},
         success : function(result){
-            if ( result.toString() == "500" ){
+            if ( result === "500" ){
                 console.log("server error")
             } else {
-                return result.toString() ;
+                return result ;
             }
             ///alert(yearMonth.toString()) ;
         }
