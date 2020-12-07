627f12f768ff8b39a5476f7401c864d32047cde0
Juminiy <2285151098@qq.com>
2020-12-07 22:02:22 +0800

    add http request

--- MyAlgorithmLibrary/MyStackArray.js
+++ MyAlgorithmLibrary/MyStackArray.js
@@ -276,7 +276,7 @@ StackArray.prototype.fieldCallback = function (event){
         this.pushFieldFind.value = "" ;
         console.log("正在查找...")
         var findResult = getPropertyByName(findValue) ;
-        if(findResult.toString() != "" ){
+        if(findResult.toString() !== "" ){
             alert("找到了 : "+ findResult) ;
         }else {
             alert("没找到这个日期的开销记录")
