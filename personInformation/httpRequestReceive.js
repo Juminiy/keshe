c68c5e2e7490a7b594ca7e3ecfd47ab9c5f4a42c
Juminiy <2285151098@qq.com>
2020-12-08 15:42:55 +0800

    finish the StackArray

--- personInformation/httpRequestReceive.js
+++ personInformation/httpRequestReceive.js
@@ -4,7 +4,7 @@ function getRecordByYearMonth(yearMonth){
         url: "https://authorize.hulingnan.site:8124/keshe/getRecord",
         data: { yearMonth:yearMonth.toString()},
         success : function(result){
-            if ( result.toString() == "no" ){
+            if ( result.toString() === "no" ){
                 alert("没有查询到此纪录") ;
             } else {
                 alert(result.toString()) ;
@@ -22,8 +22,8 @@ function addRecord(yearMonth, income, foodConsumption,
         data: { yearMonth:yearMonth.toString(),income:income.toString(),foodConsumption:foodConsumption.toString(), rent:rent.toString(),
                 childrenEducationCost:childrenEducationCost.toString(),WaterPowerCost:WaterPowerCost.toString(),MedicalCost:MedicalCost.toString() },
         success : function(result){
-            if ( result.toString() == "yes" ){
-                alert(" 添加成功! ") ;
+            if ( result === "yes" ){
+                alert("添加成功!") ;
             } else {
                 alert("添加失败!") ;
             }
@@ -37,7 +37,7 @@ function getAllRecord(){
         url: "https://authorize.hulingnan.site:8124/keshe/getAllRecord",
         data: {},
         success : function(result){
-            if ( result.toString() == "no" ){
+            if ( result.toString() === "no" ){
                 alert("账单为空！") ;
             } else {
                 alert(result.toString()) ;
@@ -51,7 +51,7 @@ function deleteRecord(yearMonth){
         url: "https://authorize.hulingnan.site:8124/keshe/deleteRecord",
         data: { yearMonth:yearMonth.toString() },
         success : function(result){
-            if ( result.toString() == "no"){
+            if ( result.toString() === "no"){
                 alert("没有此记录！") ;
             } else {
                 alert("删除记录成功！") ;
@@ -65,7 +65,7 @@ function correctRecordByYearMonth(yearMonth,propertyName,propertyValue){
         url: "https://authorize.hulingnan.site:8124/keshe/correctRecord",
         data: { yearMonth:yearMonth.toString(),propertyName:propertyName.toString(),propertyValue:propertyValue.toString()},
         success : function(result){
-            if ( result.toString() == "no"){
+            if ( result.toString() === "no"){
                 alert("没有此记录!") ;
             } else {
                 alert("修改成功!") ;
